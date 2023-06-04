28369ec6158e63ff644aeec8ebf1d0980979b722
boopalaarul <boopalaarul@yahoo.com>
2023-06-04 06:06:32 +0000

    benchmarking data successfully rsem'd

--- benchmarking/rsem_script.sh
+++ benchmarking/rsem_script.sh
@@ -1,11 +1,13 @@
-#RSEMOUT=RSEM/RSEM
+RSEMINDEX=$1/RSEM
+BAM=$2
 #rsem-prepare-reference \
 #    --gtf ${GTF} ${REFFA} ${RSEMOUT}
-
+mkdir RSEM_results
 rsem-calculate-expression \
     -p 5 \
     --fragment-length-mean -1 \
     --seed-length 25 \
-    --bam SRR23147610Aligned.toTranscriptome.out.bam \
-    ${RSEMOUT} \
-    ../RSEM_results/SRR23147610
+    --bam ${BAM} \
+    ${RSEMINDEX} \
+    RSEM_results/example \
+    >RSEM_results/log.out 2>RSEM_results/log_warn_error.out

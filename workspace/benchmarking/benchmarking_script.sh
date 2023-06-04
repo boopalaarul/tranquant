a1220bb54b32a7ece1dd649a28fb800adfe94978
boopalaarul <boopalaarul@yahoo.com>
2023-06-04 09:07:33 +0000

    last benchmarking data

--- workspace/benchmarking/benchmarking_script.sh
+++ workspace/benchmarking/benchmarking_script.sh
@@ -3,24 +3,40 @@ tail -n +2 ../RSEM_results/example.genes.results | wc -l
 echo "Number of unique genes in RSEM results"
 tail -n +2 ../RSEM_results/example.genes.results | awk '{print $1}' | uniq -c | wc -l
 echo "Number of genes with TPM > 0 in RSEM results"
-tail -n +2 ../RSEM_results/example.genes.results | awk '$6>0 {print}' | wc -l
+tail -n +2 ../RSEM_results/example.genes.results | awk '$6>0 {print $1}' > rsem_genes.txt
+wc -l rsem_genes.txt
 
 echo "Number of genes in my results"
 tail -n +2 ../myexample/example.genes.results | wc -l
 echo "Number of unique genes in my results"
 tail -n +2 ../myexample/example.genes.results | awk '{print $1}' | uniq -c | wc -l
 echo "Number of genes with TPM > 0 in my results"
-tail -n +2 ../myexample/example.genes.results | awk '$6>0 {print}' | wc -l
+tail -n +2 ../myexample/example.genes.results | awk '$6>0 {print $1}' > my_genes.txt
+wc -l my_genes.txt
 
-tail -n +2 ../RSEM_results/example.genes.results | awk '$6>0 {print $1}' > tmp.txt
-tail -n +2 ../myexample/example.genes.results | awk '{print $1}' >> tmp.txt
+cp rsem_genes.txt tmp.txt
+cat my_genes.txt >> tmp.txt
 sort tmp.txt | uniq -c > all_expressed_genes.txt
 rm tmp.txt
 
 echo "Number of genes that occur in both files"
-awk '$1=2 {print $2}' all_expressed_genes.txt > genes_in_both_files.txt
+awk '$1>1 {print $2}' all_expressed_genes.txt > genes_in_both_files.txt
 wc -l genes_in_both_files.txt
-echo "Number of genes that occur in only one files"
-awk '$1=1 {print $2}' all_expressed_genes.txt > genes_in_one_file.txt 
-wc -l genes_in_one_file.txt
+echo "Number of genes that occur only in RSEM results"
+cp genes_in_both_files.txt tmp.txt
+cat rsem_genes.txt >> tmp.txt
+sort tmp.txt | uniq -c > rsem_genes.txt
+rm tmp.txt
+awk '$1==1 {print $2}' rsem_genes.txt > rsem_unique_genes.txt 
+wc -l rsem_unique_genes.txt
+echo "Number of genes that occur only in my results"
+cp genes_in_both_files.txt tmp.txt
+cat my_genes.txt >> tmp.txt
+sort tmp.txt | uniq -c > my_genes.txt
+rm tmp.txt
+awk '$1==1 {print $2}' my_genes.txt > my_unique_genes.txt
+wc -l my_unique_genes.txt
+#echo "Number of genes that occur in only one files"
+#awk '$1==1 {print $2}' all_expressed_genes.txt > genes_in_one_file.txt 
+#wc -l genes_in_one_file.txt
 
--- workspace/benchmarking/shared_genes.py
+++ workspace/benchmarking/shared_genes.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-output_df = pd.DataFrame(columns=["gene_id", "transcript_id(s) [RSEM]", "transcript_id(s) [tranquant]", "length [RSEM]", "length [tranquant]" "TPM [RSEM]", "TPM [tranquant]"])
+output_df = pd.DataFrame(columns=["gene_id", "tx_id(s) [RSEM]", "tx_id(s) [tranquant]", "tx_id_agreement", "length [RSEM]", "length [tranquant]", "TPM [RSEM]", "TPM [tranquant]"])
 
 """step 1: count all gene ids that occur in both files"""
 
@@ -10,7 +10,7 @@ for line in file:
     gene_set.add(line.strip(" \t\n"))
 file.close()
 
-print(len(gene_set))
+#print(len(gene_set))
 """step 2: read RSEM results"""
 rsem_results = open("../RSEM_results/example.genes.results", "r")
 
@@ -22,9 +22,9 @@ for line in rsem_results:
     data = line.strip("\";\n").split("\t")
     if data[0] not in gene_set:
         continue
-    rsem_tx_ids.update({line : data[1]})
-    rsem_length.update({line : data[2]})
-    rsem_tpm.update({line : data[5]})
+    rsem_tx_ids.update({data[0] : data[1]})
+    rsem_length.update({data[0] : data[2]})
+    rsem_tpm.update({data[0] : data[5]})
 rsem_results.close()
 
 """step 3: read my results"""
@@ -38,19 +38,20 @@ for line in my_results:
     data = line.strip("\";\n").split("\t")
     if data[0] not in gene_set:
         continue
-    my_tx_ids.update({line : data[1]})
-    my_length.update({line : data[2]})
-    my_tpm.update({line : data[5]})
+    my_tx_ids.update({data[0] : data[1]})
+    my_length.update({data[0] : data[2]})
+    my_tpm.update({data[0] : data[5]})
 my_results.close()
 
 """step 4: calculate metrics"""
-
+#print(len(rsem_tx_ids.keys()))
+#print(len(my_tx_ids.keys()))
+tx_id_equality = {}
 for gene in gene_set:
-    output_df.loc[len(output_df.index)] = [gene, rsem_tx_ids[gene], my_tx_ids[gene], rsem_length[gene], my_length[gene], rsem_tpm[gene], my_tpm[gene]]
+    
+    tx_id_equality.update({gene : (set(rsem_tx_ids[gene].strip("\";\n").split(",")) == set(my_tx_ids[gene].strip("\";\n").split(",")))})
+    output_df.loc[len(output_df.index)] = [gene, rsem_tx_ids[gene], my_tx_ids[gene], tx_id_equality[gene], rsem_length[gene], my_length[gene], rsem_tpm[gene], my_tpm[gene]]
 
-#"""step 5: TPM for each gene, it's the RPKM for that gene / S"""
-#for column in dataframe
-#take each rpk make a tpm
-#output_df["TPM"] = output_df["RPK"] / normalization_factor
+"""step 5: tx_id list equality check"""
 
 output_df.to_csv(path_or_buf="shared_genes_comparison.txt", sep='\t', header=True, index=False)
